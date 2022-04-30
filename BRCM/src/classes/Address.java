8b750d79a8b0f690639e182245bd094a57e4a1c9
Omar Rodriguez <eomarrodriguez@outlook.com>
2022-04-30 02:30:58 -0700

    Add directory to keep classes for tidiness.

---
+++ BRCM/src/classes/Address.java
@@ -0,0 +1,16 @@
+package classes;
+public class Address {
+    private String street;
+    private int unitNumber;
+    private String city;
+    private String state;
+    private int zipCode;
+
+    public Address(String street, int unitNumber, String city, String state, int zipCode) {
+        this.street = street;
+        this.unitNumber = unitNumber;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+}
---
+++ BRCM/src/classes/Customer.java
@@ -0,0 +1,45 @@
+package classes;
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Customer {
+    protected enum Status {
+        STUDENT, PROFESSOR, BOTH
+    }
+
+    private int broncoID;
+    private String name;
+    private String phoneNumber;
+    private Address address;
+    private LocalDate DOB;
+    private float discountScheme = (float) 0.0;
+    private Status status = null;
+
+    public Customer(int broncoID, String name, String phoneNumber, String street,
+            int unitNumber, String city, String state, int zipCode,
+            int year, Month month, int day) {
+        this.broncoID = broncoID;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = new Address(street, unitNumber, city, state, zipCode);
+        this.DOB = LocalDate.of(year, month, day);
+    }
+
+    public void setDiscountScheme(Status status) {
+        if (status == Status.BOTH) {
+            this.discountScheme = (float) 0.25;
+        } else if (status == Status.STUDENT) {
+            this.discountScheme = (float) 0.20;
+        } else if (status == Status.PROFESSOR) {
+            this.discountScheme = (float) 0.15;
+        }
+    }
+
+    public float getDiscountScheme() {
+        return this.discountScheme;
+    }
+
+    public int getBroncoID() {
+        return this.broncoID;
+    }
+}
