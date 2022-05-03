cec99ce3d2cebaa91af03c5d5db1d8c645b0ecd7
Omar Rodriguez <eomarrodriguez@outlook.com>
2022-05-02 21:10:01 -0700

    Implement Go Back button functionality

--- BRCM/src/control/SignUpController.java
+++ BRCM/src/control/SignUpController.java
@@ -1,5 +1,50 @@
 package control;
 
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+
 public class SignUpController {
-    
+
+    // FXML links
+    @FXML
+    private Button singUMeUpButton;
+    @FXML
+    private Button goBackButton;
+    @FXML
+    private TextField broncoIDTextField;
+    @FXML
+    private TextField passwordTextField;
+    @FXML
+    private TextField fullNameTextField;
+    @FXML
+    private TextField phoneNumberTextField;
+    @FXML
+    private TextField streetTextField;
+    @FXML
+    private TextField zipCodTextField;
+    @FXML
+    private TextField cityTextField;
+
+    // variables
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void SwitchToLoginScene(ActionEvent event) throws IOException {
+        this.root = FXMLLoader.load(getClass().getResource("../view/LogInView.fxml"));
+        this.stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+        this.scene = new Scene(this.root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
