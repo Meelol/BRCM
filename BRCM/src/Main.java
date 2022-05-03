c725d2c93d930f0e14af85d40ce3552ed3322247
Omar Rodriguez <eomarrodriguez@outlook.com>
2022-05-02 18:45:39 -0700

    Start work on viewws.

--- BRCM/src/Main.java
+++ BRCM/src/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/LoginView.fxml"));
-        Scene scene = new Scene(root,800,800);
+        Scene scene = new Scene(root,800,600);
         primaryStage.setTitle("Bronco Recreational Complex Management");
         primaryStage.setScene(scene);
         primaryStage.show();
