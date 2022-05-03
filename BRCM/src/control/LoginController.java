fbf0499accaaa8285b04e6286ad812433547c5cd
Omar Rodriguez <eomarrodriguez@outlook.com>
2022-05-02 20:18:45 -0700

    Added controller classes, implement part of LoginController

---
+++ BRCM/src/control/LoginController.java
@@ -0,0 +1,39 @@
+package control;
+
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button singUpButton;
+    @FXML
+    private Button logInButton;
+    @FXML
+    private TextField broncoIDTextField;
+    @FXML
+    private PasswordField passwordField;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToSignUpScene(ActionEvent event) throws IOException {
+        System.out.println("HOLA");
+        this.root = FXMLLoader.load(getClass().getResource("../view/SignUpView.fxml"));
+        this.stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+        this.scene = new Scene(this.root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
