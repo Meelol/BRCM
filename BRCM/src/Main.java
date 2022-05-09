8dcbab96325305f942de227b6f889fd4e8c16e2c
Omar Rodriguez <eomarrodriguez@outlook.com>
2022-05-08 19:55:34 -0700

    Add image to res, work on ProductsView, add DOB textfield in SignUpView

--- BRCM/src/Main.java
+++ BRCM/src/Main.java
@@ -6,24 +6,23 @@ import javafx.stage.Stage;
 
 import java.sql.*;
 
-
 public class Main extends Application {
 
     public static void main(String[] args) {
-            // Launch GUI
-            launch(args);
+        // Launch GUI
+        launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/LoginView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/ProductsView.fxml"));
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setTitle("Bronco Recreational Complex Management");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    public static Connection shareConn(Connection conn){
+    public static Connection shareConn(Connection conn) {
         return conn;
     }
 }
