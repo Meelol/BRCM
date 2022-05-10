1d574a37194b7bb8c81b95748b86929e78b1abad
Omar Rodriguez <eomarrodriguez@outlook.com>
2022-05-10 12:26:38 -0700

    Add Activities Model and Class

---
+++ BRCM/src/model/classes/Activity.java
@@ -0,0 +1,19 @@
+package model.classes;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+
+public class Activity {
+    private int productID;
+    private String name;
+    private float price;
+    private String date;
+
+    private Button button;
+    private DatePicker datePicker;
+
+    public Activity(int productID, String name, float price, String date) {
+        
+    }
+
+}
